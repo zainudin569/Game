a9544493f7594c8a28c031689722942d62638626
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-06-19 21:49:49 +0300

    C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.cpp -o C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Game.cpp
+++ Ozhaev Zaynudin Game.cpp
@@ -253,15 +253,15 @@ void CollisionBall (Ball* ball_1, Ball* ball_2)
         double Vn1 = (*ball_2) .vx*sin + (*ball_2) .vy*cos; //поворот системы координат шар1
         double Vn2 = (*ball_1) .vx*sin + (*ball_1) .vy*cos; //поворот системы координат шар2
 
-        //if ((Vn1 - Vn2) == 0) Vn1 = Vn1 + 0.01;
+        if ((Vn1 - Vn2) == 0) Vn1 = Vn1 + 0.01;
 
-        /*double dt = ((*ball_1) .r + (*ball_2) .r - d)/(Vn1 - Vn2); // удаление залипания
+        double dt = ((*ball_1) .r + (*ball_2) .r - d)/(Vn1 - Vn2); // удаление залипания
 
         if (dt >  1)  dt = 1;   // ограничение на dt, чтоб мяч не отскакивал далеко
         if (dt < -1) dt = -1;
 
-        //(*ball_1) .x = ROUND((*ball_1) .x - (*ball_1) .vx*dt);
-        //(*ball_1) .y = ROUND((*ball_1) .y - (*ball_1) .vy*dt);
+        (*ball_1) .x = ROUND((*ball_1) .x - (*ball_1) .vx*dt);
+        (*ball_1) .y = ROUND((*ball_1) .y - (*ball_1) .vy*dt);
         (*ball_2) .x = ROUND((*ball_2) .x - (*ball_2) .vx*dt);
         (*ball_2) .y = ROUND((*ball_2) .y - (*ball_2) .vy*dt);
 
@@ -272,23 +272,25 @@ void CollisionBall (Ball* ball_1, Ball* ball_2)
         cos = Dy/d; // cos
         Vn1 = (*ball_2) .vx*sin + (*ball_2) .vy*cos;
         Vn2 = (*ball_1) .vx*sin + (*ball_1) .vy*cos;
-        */
+
         double Vt1 = -(*ball_2) .vx*cos + (*ball_2) .vy*sin; //поворот системы координат шар1
         double Vt2 = -(*ball_1) .vx*cos + (*ball_1) .vy*sin; //поворот системы координат шар2
 
-        double o = Vn2; //меняем местами vn1 и vn2
+        /*double o = Vn2; //меняем местами vn1 и vn2
         Vn2 = Vn1;
         Vn1 = o;
+        */
+        double Vn2= -(*ball_2) .vx*cos+(*ball_2) .vy*sin;
 
-        Vn1 = Vn1 - Vn2;
+          Vn2 = Vn1 - Vn2;
 
         //(*ball_1) .vx = ROUND(Vn2*sin - Vt2*cos); //обратный поворот системы координат шар1
         //(*ball_1) .vy = ROUND(Vn2*cos + Vt2*sin); //обратный поворот системы координат шар1
         (*ball_2) .vx = ROUND(Vn1*sin - Vt1*cos); //обратный поворот системы координат шар2
         (*ball_2) .vy = ROUND(Vn1*cos + Vt1*sin); //обратный поворот системы координат шар2
 
-        //(*ball_1) .x = ROUND((*ball_1) .x + (*ball_1) .vx*dt);
-        //(*ball_1) .y = ROUND((*ball_1) .y + (*ball_1) .vy*dt);
+        (*ball_1) .x = ROUND((*ball_1) .x + (*ball_1) .vx*dt);
+        (*ball_1) .y = ROUND((*ball_1) .y + (*ball_1) .vy*dt);
         (*ball_2) .x = ROUND((*ball_2) .x + (*ball_2) .vx*dt);
         (*ball_2) .y = ROUND((*ball_2) .y + (*ball_2) .vy*dt);
         //printf (" dt  = %f\n", dt);
