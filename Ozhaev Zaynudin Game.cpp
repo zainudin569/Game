b52961dc222768f209004de6f602535428882e0d
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-06-20 14:37:59 +0300

    C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.cpp -o C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Game.cpp
+++ Ozhaev Zaynudin Game.cpp
@@ -208,7 +208,7 @@ void Ball::Physics(int* score1, int* score2, int dt)
 
 void CollisionBall (Ball* ball_p, Ball* ball_b)
     {
-    /*int Dx = (*ball_b) .x - (*ball_p) .x; // стороны треугольника
+    int Dx = (*ball_b) .x - (*ball_p) .x; // стороны треугольника
     int Dy = (*ball_b) .y - (*ball_p) .y; // стороны треугольника
     double d = sqrt(Dx*Dx + Dy*Dy); if (d == 0) d = 0.01; //гипотенуза
     double sin = Dx/d; // sin угла треугольника
@@ -254,61 +254,7 @@ void CollisionBall (Ball* ball_p, Ball* ball_b)
         (*ball_b) .y = ROUND((*ball_b) .y + (*ball_b) .vy*dt);
         //printf (" dt  = %f\n", dt);
         //txSleep (3000);
-        }*/
-
-     double Dx = (*ball_b) .x - (*ball_p) .x;
-     double Dy = (*ball_b) .y - (*ball_p) .y;
-     double d = sqrt(Dx*Dx + Dy*Dy); if (d==0) d = 0.01;   // во избежании деления на ноль
-	 double sin = Dx/d;
-	 double cos = Dy/d;
-
-	 if (d < (*ball_b) .r+(*ball_p) .r)
-	 {
-         txPlaySound ("sounds/Zvuk_Ball.wav");
-
-		 double Vn1 = (*ball_p).vx*sin + (*ball_p).vy*cos;
-         double Vn2 = (*ball_b).vx*sin + (*ball_b).vy*cos;
-
-		 if ((Vn1 - Vn2) == 0) Vn1 = Vn1 + 0.01;
-
-		 double dt=((*ball_b).r+(*ball_p).r-d)/(Vn1-Vn2);
-
-	                 if (dt>0.6)  dt= 0.6;   // ограничение на dt, чтоб мяч не улетал
-                     if (dt<-0.6) dt=-0.6; // усли проникновение сильное было
-
-
-		 (*ball_b).x-=ROUND((*ball_b).vx*dt);
-		 (*ball_b).y-=ROUND((*ball_b).vy*dt);
-		 (*ball_p).x-=ROUND((*ball_p).vx*dt);
-		 (*ball_p).y-=ROUND((*ball_p).vy*dt);
-
-
-
-	////////основная часть, находим новые скорости после столкновения
-		  Dx = (*ball_b).x-(*ball_p).x;
-		  Dy = (*ball_b).y-(*ball_p).y;
-	      d = sqrt(Dx*Dx+Dy*Dy);  if (d==0) d=0.01;
-  		  sin = Dx/d;
-		  cos = Dy/d;
-
-
-		  Vn1=(*ball_p).vx*sin+(*ball_p).vy*cos;
-          Vn2=(*ball_b).vx*sin+(*ball_b).vy*cos;
-
-   double Vt2= -(*ball_b).vx*cos+(*ball_b).vy*sin;
-
-          Vn2 = Vn1-Vn2;
-
-          (*ball_b).vx=ROUND(Vn2*sin-Vt2*cos);
-		  (*ball_b).vy=ROUND(Vn2*cos+Vt2*sin);
-    ///////
-
-
-		 (*ball_b).x+=ROUND((*ball_b).vx*dt);
-		 (*ball_b).y+=ROUND((*ball_b).vy*dt);
-		 (*ball_p).x+=ROUND((*ball_p).vx*dt);
-		 (*ball_p).y+=ROUND((*ball_p).vy*dt);
-	 }
+        }
 
 }
 
