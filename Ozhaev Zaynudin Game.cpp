0a0423f4cfa9d63a077db200cfeca8aa8156bf2f
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-06-20 13:57:59 +0300

    C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.cpp -o C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Game.cpp
+++ Ozhaev Zaynudin Game.cpp
@@ -171,7 +171,7 @@ void Key::Control(Ball* ball, int* F4_Col)
 
 //---------------------------------------------------------------------------------
 
-void Ball::Physics(int* score1, int* score2, int dt, int p)
+void Ball::Physics(int* score1, int* score2, int dt)
     {
     if (vy >  15) vy =  15;//ограничение на скорость движения
     if (vx >  15) vx =  15;
