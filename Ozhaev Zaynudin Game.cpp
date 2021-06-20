85cf189f0a8b302e058dc7a4936e4524942d5ac9
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-06-20 12:01:24 +0300

    C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.cpp -o C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Game.cpp
+++ Ozhaev Zaynudin Game.cpp
@@ -280,10 +280,10 @@ void CollisionBall (Ball* ball_p, Ball* ball_b)
         (*ball_b) .vx = ROUND(Vn2*sin - Vt2*cos); //обратный поворот системы координат шар2
         (*ball_b) .vy = ROUND(Vn2*cos + Vt2*sin); //обратный поворот системы координат шар2
 
-        (*ball_p) .x = ROUND((*ball_p) .x + (*ball_p) .vx*dt);
-        (*ball_p) .y = ROUND((*ball_p) .y + (*ball_p) .vy*dt);
-        (*ball_b) .x = ROUND((*ball_b) .x + (*ball_b) .vx*dt);
-        (*ball_b) .y = ROUND((*ball_b) .y + (*ball_b) .vy*dt);
+        (*ball_b).x+=(*ball_b).vx*dt;
+		 (*ball_b).y+=(*ball_b).vy*dt;
+		 (*ball_p).x+=(*ball_p).vx*dt;
+		 (*ball_p).y+=(*ball_p).vy*dt;
         //printf (" dt  = %f\n", dt);
         //txSleep (3000);
         }
