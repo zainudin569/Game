445dd7cb81b9d5e693a7f889fb5973ee88953cee
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-06-20 13:32:54 +0300

    C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.cpp -o C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Game.cpp
+++ Ozhaev Zaynudin Game.cpp
@@ -160,8 +160,10 @@ void Key::Control(Ball* ball, int* F4_Col, int dt)
         if (*F4_Col == 1)
             {
 
-        if (vy >  25) vy =  25;  if (vx >  25) vx =  25;
-        if (vy < -25) vy = -25;  if (vx < -25) vx = -25;   //ограничение на скорость движения
+        if ((*ball) .vy >  25) (*ball) .vy =  25;//ограничение на скорость движения
+        if ((*ball) .vx >  25) (*ball) .vx =  25;
+        if ((*ball) .vy < -25) (*ball) .vy = -25;
+        if ((*ball) .vx < -25) (*ball) .vx = -25;
 
             (*ball) .Color     = RGB((*ball) .x,   (*ball) .y,   150);
             (*ball) .FillColor = RGB((*ball) .x/2, (*ball) .y/2, 150);
