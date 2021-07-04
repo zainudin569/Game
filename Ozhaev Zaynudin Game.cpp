426c5aa9291ebd2297ef2ed8c935ae711b422de7
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-07-05 02:19:33 +0300

    C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.cpp -o C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Game.cpp
+++ Ozhaev Zaynudin Game.cpp
@@ -60,13 +60,13 @@ void MoveBall()
     //srand(time(NULL));
 
     Ball ball1 = { W/2 + rand() % W/2 ,  rand() % H, 0, 0,
-                   40,
+                   35,
                    1,
                    RGB(50 + rand() % 200, 50 + rand() % 200, 50 + rand() % 200),
                    RGB(50 + rand() % 200, 50 + rand() % 200, 50 + rand() % 200) };
 
     Ball ball2 = { rand() % W/2,  rand() % H, 0, 0,
-                   40,
+                   35,
                    2,
                    RGB(50 + rand() % 200, 50 + rand() % 200, 50 + rand() % 200),
                    RGB(50 + rand() % 200, 50 + rand() % 200, 50 + rand() % 200) };
