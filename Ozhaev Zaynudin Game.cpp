b7805a631a4468cc99bf693f4c62327dc167c420
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-07-04 11:50:44 +0300

    C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.cpp -o C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Game.cpp
+++ Ozhaev Zaynudin Game.cpp
@@ -164,14 +164,14 @@ void MoveBall()
         ball1.Drow();
         ball2.Drow();
         //ball3.Drow();
-        //txBitBlt (txDC(), ball3 .x - 25, ball3 .y - 25, 50, 50, Ball, 0, 0);
+        txBitBlt (txDC(), ball3 .x - 25, ball3 .y - 25, 50, 50, Ball, 0, 0);
         //txAlphaBlend (txDC(), ball3 .x - 25, ball3 .y - 25, 50, 50, Ball, 0, 0, TX_BLACK);
 
         int i = 0;
         //int j = 0;
         int sizeX = txGetExtentX (Photo .Ball);
         //int sizeY = txGetExtentY (Photo .Ball);
-        txAlphaBlend (txDC(), ball3 .x - 25, ball3 .y - 25, 0, 0, Photo .Ball, 0, 0);
+        //txAlphaBlend (txDC(), ball3 .x - 25, ball3 .y - 25, 0, 0, Photo .Ball, 0, 0);
 
         ScoreDraw (score1, score2);
 
