6eccb3d919b7056bcaa9dcbc34c939f451ba2d36
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-07-05 01:58:35 +0300

    C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.cpp -o C:\Users\zainu\Desktop\курс\Game\Ozhaev Zaynudin Game.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Game.cpp
+++ Ozhaev Zaynudin Game.cpp
@@ -174,7 +174,7 @@ void MoveBall()
         if (i >= 7) i = 0;
 
         txAlphaBlend (txDC(), ball1 .x - 60, ball1 .y - 85, 150, 150, Photo .Pad, 0, 150);
-        txAlphaBlend (txDC(), ball2 .x - 70, ball2 .y - 90, 150, 150, Photo .Pad, 150, 0);
+        txAlphaBlend (txDC(), ball2 .x - 75, ball2 .y - 90, 150, 150, Photo .Pad, 150, 0);
 
         if (ball1 .x <= H/2)
             {
